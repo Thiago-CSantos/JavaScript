7580fa7ec31d80cf534ae0d050cf5200f025acb5
Thiago Carretero <thiagocarretero433@gmail.com>
2023-03-05 22:28:44 -0300

    Condições (Parte 1) - Curso JavaScript #11

---
+++ cursoJS/aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 50.69
+
+console.log("A velocidade do seu carro é: "+vel)
+
+if(vel>60.99){
+    console.log(`Você ultrapassou a velocidade permitida`);
+}console.log("Dirija sempre com segurança");
