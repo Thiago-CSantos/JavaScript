5d1eb49989654e07c6cf0fa4cba9753d5105a4f7
Thiago Carretero <thiagocarretero433@gmail.com>
2023-05-13 23:43:09 -0300

    ,

--- cursoJS/Curso de JavaScript e TypeScript do básico ao avançado/React/lista-de-tarefas/src/Main.js
+++ cursoJS/Curso de JavaScript e TypeScript do básico ao avançado/React/lista-de-tarefas/src/Main.js
@@ -1,14 +1,15 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from "react";
- import "./Main.css";
+import "./Main.css";
 
 // Icons para Form
-import {FaPlusCircle} from "react-icons/fa";
+import { FaPlusCircle } from "react-icons/fa";
 
 export default class Main extends Component {
 
     state = {
         novaTarefa: '',
+        trefas: ['tomar café', 'Beber água', 'estudar'],
     };
 
     handleChange = (e) => {
@@ -20,15 +21,21 @@ export default class Main extends Component {
 
     render() {
         // criação de uma variavel novaTarefa
-        const { novaTarefa } = this.state
+        const { novaTarefa, tarefas } = this.state
 
         return (
             <div className="main">
                 <h1>Lista de tarefa</h1>
                 <form action="#" className="formulario">
                     <input onChange={this.handleChange} type="text" value={novaTarefa} />
-                    <button type="submit" >{<FaPlusCircle/>}</button>
+                    <button type="submit" >{<FaPlusCircle />}</button>
                 </form>
+
+                <ul className="tarefas">
+                    {tarefas.map((value) => (
+                        <li key={tarefas}> {value} </li>
+                    ))}
+                </ul>
             </div>
         );
     }
